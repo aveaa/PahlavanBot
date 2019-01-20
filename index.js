f85a20216068d79d0cf2418aac54edac6fc6db8b
SevenTrio <36824327+SevenTrio@users.noreply.github.com>
2019-01-20 15:18:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ client.on('message', async (message) => {
   }
 
   if(['414350089226551319', '425082092838453249', '528261560402182159'].includes(message.channel.id) && !["218719595618500608", "218656629720219658"].includes(message.author.id)){
-    multipleReact(message, ['425506799408513024', '425506818601517066'])
+    multipleReact(message, ['536534338502918148', '536534339014361108'])
   }
 
   if(message.author.bot) return;
